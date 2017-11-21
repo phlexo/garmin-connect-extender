e17cef821d6b91818b6f3cc60785667e2ae0f158
phlexo <33544015+phlexo@users.noreply.github.com>
2017-11-21 15:38:59 +0100

    Example usage of using options.

--- content/widget-size.js
+++ content/widget-size.js
@@ -1,2 +1,16 @@
 (function ($) {
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    function onGot(item) {
+        var widgetSize = -1;
+        if (item.widget.size) {
+            widgetSize = item.widget.size;
+        }
+        console.log(`Setting widget size to ${widgetSize}px`);
+    }
+
+    var getting = browser.storage.local.get();
+    getting.then(onGot, onError);
 })(jQuery);
