e6a8ad8743471db0b4eb105e90a514068c049d1b
Andreas Lindström <Andreas.Lindstrom@powel.se>
2018-01-18 16:01:34 +0100

    Added some urls.

--- dashboard/background.js
+++ dashboard/background.js
@@ -1,7 +1,20 @@
 (function ($) {
     browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-        if (tab.url.match(/https?:\/\/connect.garmin.com\/modern[\/]?[dashboard\/.*]?/gi)) {
+        if (tab.url.match(/https?:\/\/connect.garmin.com\/modern\/dashboard.*/gi)) {
             console.log(`Laddar aktiviteter för tab med id ${tab.id}.`)
+
+            // Årlig status
+            // https://connect.garmin.com/modern/proxy/calendar-service/year/2018
+
+            // Januari
+            // https://connect.garmin.com/modern/proxy/calendar-service/year/2018/month/0
+
+            // Vecka
+            // https://connect.garmin.com/modern/proxy/calendar-service/year/2018/month/0/day/18/start/1
+
+            // Enskild aktivitet
+            // https://connect.garmin.com/modern/proxy/activity-service/activity/2434047486?_=1516287552093
+
             $.ajax({url: "https://connect.garmin.com/modern/proxy/activitylist-service/activities/phlexo?start=1&limit=30&_=1516279328566", success: function(result) {
                 console.log(`Aktiviteter laddade, skickar aktiviterna till content skriptet till tab med id ${tab.id}.`);
                 console.log(result);
